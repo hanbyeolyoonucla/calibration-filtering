c5812902e691fcaa03b957d17b0abb76b4475614
unknown <hbyoon1@g.ucla.edu>
2023-04-25 11:47:08 -0700

    Update dh filtering

--- dh_filtering.m
+++ dh_filtering.m
@@ -18,16 +18,27 @@ DH_nominal = ...
     0 0 0 pi/2 0;
     70 pi 0 -pi/2 0]; % d theta a alpha
 
+% calibrated DH from Kalibrot
+calibratedDH = [0	0	0	135;
+-1.567671326	-0.09418696603	-1.567522278	0.6327129301;
+0.00232117105	134.99947	-0.001921290774	0;
+-1.57539267	37.78467258	0.004128683254	119.8986164;
+1.572228153	-0.04966838003	-0.0007294401206	-0.003682920773;
+-1.567004188	0.0002605447373	3.141592654	70]; % alpha a theta d 
+
 % calibrated DH from RoboDK
-calibratedDH = [0.0000000,-1.5685302,-0.0015844,-1.5710132,1.5713967,-1.5703714;
-0.0000000,0.3760858,134.9888038,37.8811929,0.0460869,0.0019857;
-0.0000000,-1.5714982,0.0019691,0.0000571,0.0018524,3.1415927;
-135.0000000,0.1216121,0.0000000,120.0658861,-0.0415388,70.0000000]'; % alpha a theta d 
+% calibratedDH = [0.0000000,-1.5685302,-0.0015844,-1.5710132,1.5713967,-1.5703714
+% 0.0000000,0.3760858,134.9888038,37.8811929,0.0460869,0.0019857
+% 0.0000000,-1.5714982,0.0019691,0.0000571,0.0018524,3.1415927
+% 135.0000000,0.1216121,0.0000000,120.0658861,-0.0415388,70.0000000]';
 
 % tcp information
-TCP = [-0.4590164281	-0.9164464634	161.3819234	168.2432879	85.64040621	11.58103583];
+TCP = [-0.5732614908	-0.1687166436	158.138543	170.0399912	85.77777461	9.550385364];
 T_tool_rdk = XYZWPR2SE3(TCP);
-% T_tool_rdk = eye(4,4);
+% T_tool_rdk = [1	0	0	24.29335976
+% 0	1	0	-9.705854952
+% 0	0	1	54.81243107
+% 0	0	0	1];
 
 % DH following convention: theta,d,a,alpha
 cdh = calibratedDH(:, [3 4 2 1]);
@@ -37,38 +48,44 @@ nq = ndh(:,1)'; % zero config
 
 % define robot
 for ii = 1:6
-    cL(ii) = Revolute('d', cdh(ii,2), 'a', cdh(ii,3), 'alpha', cdh(ii,4),'modified');
-    nL(ii) = Revolute('d', ndh(ii,2), 'a', ndh(ii,3), 'alpha', ndh(ii,4),'modified');
+    cL(ii) = Revolute('d', cdh(ii,2), 'a', cdh(ii,3), 'alpha', cdh(ii,4),'offset', cdh(ii,1),'modified');
+    nL(ii) = Revolute('d', ndh(ii,2), 'a', ndh(ii,3), 'alpha', ndh(ii,4),'offset', ndh(ii,1),'modified');
 end
 crobot = SerialLink(cL,'tool',T_tool_rdk,'name',"Final Robot");
 nrobot = SerialLink(nL,'tool',T_tool_rdk,'name',"Final Robot");
 
 % robot figure
-figure()
-title('Calibrated Robot')
-crobot.plot(cq)
-figure()
-title('Nominal Robot')
-nrobot.plot(nq)
+% figure()
+% title('Calibrated Robot')
+% crobot.plot(cq)
+% figure()
+% title('Nominal Robot')
+% nrobot.plot(nq)
 
 %% 2. Filtering: calibrated IK and nominal FK
 
 % pre-filtered trajectory
-traj_prefilt = readmatrix('./data/typo_1.2A-7/Unfiltered_occ_01.txt'); % x y z Rx Ry Rz [mm,deg]
+traj_prefilt = readmatrix('./data/typo_1.2F-4/Unfiltered_occ_01.txt'); % x y z Rx Ry Rz [mm,deg]
+% traj_prefilt = [];
 Ts_prefilt = XYZWPR2SE3(traj_prefilt);
 N = size(traj_prefilt,1);
 
 % initial guess
-q0 = [9.12000000000000	-11.5900000000000	48.5200000000000	-84.7500000000000	-82.7600000000000	97.7500000000000]*pi/180;
-q0 = cq + q0;
+q0 = [4.562069,38.431293,17.880776,-94.485517,-100.693966,64.669828]*pi/180;
+% q0 = cq + q0;
+
+% for ii = 1:N
+%     joint_filtered(ii,:) = crobot.ikine(Ts_prefilt(:,:,ii),'q0',q0);
+%     disp(joint_filtered(ii,:))
+% end
 
 % filter trajectory: calibrated IK
 joint_filtered = crobot.ikine(Ts_prefilt,'q0',q0);
-joint_filtered = joint_filtered - cq;
-joint_filtered = joint_filtered + [0 0 0 0 0 2*pi];
+% joint_filtered = joint_filtered - cq;
+% joint_filtered = joint_filtered + [0 0 0 0 0 2*pi];
 
 % filter trajectory: nominal FK
-Ts_filtered = nrobot.fkine(nq + joint_filtered);
+Ts_filtered = nrobot.fkine(joint_filtered);
 p_filtered = transl(Ts_filtered);
 
 for ii = 1:length(Ts_filtered)
@@ -95,7 +112,12 @@ ylabel('y')
 zlabel('z')
 axis equal
 
+
 % save the result
-writematrix(traj_filtered, './result/typo_1.2A-7/occ_01_petercorke.txt','Delimiter','tab')
+writematrix(traj_filtered, './result/typo_1.2F-4/occ_01.txt','Delimiter','tab')
 % writematrix(joint_filtered*180/pi, './result/axial_path_filtered_joint_angles_petercorke.txt')
 
+%%
+% figure()
+% nrobot.plot(joint_filtered)
+% SE32mat(crobot.fkine([4.562069,38.431293,17.880776,-94.485517,-100.693966,64.669828]*pi/180))
